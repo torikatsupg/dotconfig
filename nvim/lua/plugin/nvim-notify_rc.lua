31a7844f679d66e4c82af19434d24da28c04e18a
torikatsu <torikatsu923@gmail.com>
2022-09-15 02:41:03 +0900

    chnge telescope theme

--- nvim/lua/plugin/nvim-notify_rc.lua
+++ nvim/lua/plugin/nvim-notify_rc.lua
@@ -1,6 +1,7 @@
 return function()
   local notify = require 'notify'
   notify.setup {
+    background_colour = "#000000"
   }
   vim.notify = notify
 end
--- nvim/lua/plugin/telescope_rc.lua
+++ nvim/lua/plugin/telescope_rc.lua
@@ -1,5 +1,13 @@
 return function()
   local telescope = require 'telescope'
+  telescope.setup {
+    pickers = {
+      find_files = {
+        theme = "dropdown",
+      }
+    },
+  }
+
   telescope.load_extension('fzf')
   telescope.load_extension("frecency")
 end
