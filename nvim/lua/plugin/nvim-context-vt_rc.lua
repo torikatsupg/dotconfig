269da677f87171068abff90514b4968a9d1d69ed
torikatsupg <torikatsu923@gmail.com>
2022-09-15 11:17:15 +0900

    disable context-vt at rust

--- nvim/lua/plugin/nvim-context-vt_rc.lua
+++ nvim/lua/plugin/nvim-context-vt_rc.lua
@@ -1,5 +1,5 @@
 return function()
   require('nvim_context_vt').setup({
-    disable_ft = { 'markdown', 'yaml', 'yml', 'dart', 'rust' },
+    disable_ft = { 'markdown', 'yaml', 'yml', 'dart', 'rs', 'ts', 'tsx', 'js' },
   })
 end
