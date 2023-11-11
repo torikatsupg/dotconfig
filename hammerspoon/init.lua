155d615e838b64d67d06e0281d082037441e394f
鳥居克哉 <toriikatsuya@toriikatsuyanoMacBook-Pro.local>
2023-11-11 21:41:37 +0900

    send eisu on escape

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,8 +1,30 @@
-hs.hotkey.bind({"command"}, "space", function()
-  local alacritty = hs.application.find('alacritty')
-  if alacritty:isFrontmost() then
-    alacritty:hide()
-  else
-    hs.application.launchOrFocus("/Applications/Alacritty.app")
-  end
-end)
+local function handleEvent(event)
+    local keyCode = event:getKeyCode()
+    local flags = event:getFlags()
+
+    if keyCode == hs.keycodes.map['['] and flags.ctrl then
+        hs.eventtap.keyStroke({}, 'eisu')
+        return false
+    end
+
+    if keyCode == hs.keycodes.map['escape'] then
+        hs.eventtap.keyStroke({}, 'eisu')
+        return false
+    end
+
+
+    -- ⌘ + space: Toggle alacritty
+    if keyCode == hs.keycodes.map['space'] and flags.cmd then
+      local alacritty = hs.application.find('alacritty')
+      if alacritty:isFrontmost() then
+        alacritty:hide()
+      else
+        hs.application.launchOrFocus("/Applications/Alacritty.app")
+      end
+    end
+
+    return false
+end
+
+local eventtap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, handleEvent)
+eventtap:start()
